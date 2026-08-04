a4316653a554684a32f2ea5baad5ca838c6f56f4
Hatta05 <haoi0410@icloud.com>
2026-08-04 17:09:56 +0900

    Rmove !

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "hello,world!"##helloworldの文字列のhtmlレスポンスを返す
+    render html: "hello,world"##helloworldの文字列のhtmlレスポンスを返す
   end
 end
